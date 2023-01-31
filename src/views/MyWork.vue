a81d951467277a1b266e7d05f8187e7a23c8a18c
harper-amyris <100603151+harper-amyris@users.noreply.github.com>
2023-01-30 22:00:15 -0500

    copy updates

--- src/views/MyWork.vue
+++ src/views/MyWork.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- https://www.primefaces.org/primevue/timeline -->
   <TopNav/>
-  <h1 class="text-center">MY WORK</h1>
+  <h1 class="text-center">MY EXPERIENCE</h1>
   <Timeline :value="events" align="alternate" class="customized-timeline">
 	<template #marker="slotProps">
 		<span class="custom-marker" :style="{backgroundColor: slotProps.item.color}">
@@ -47,7 +47,7 @@ export default {
     data() {
         return {
             events: [
-                {company: 'Amyris', companyLogo: 'logo-amyris-colour.svg', date: '2020 - Present', blurb: 'Maintain tagging and analytics for major brands including but not limited to Biossance, JVN Hair and Pipette Baby. Migrated 50+ tags from these brands to Google Tag Manager. Build internal dashboard with Vue.JS and node to integrate with third party APIs (DataDog, Tag Inspector) to display critical website metrics, including a visual map of tags installed on the site and how they are loaded. Use this data to audit Shopify stores to improve site speed and overall performance (boost core web vital scores from 40-50 to 80). Give presentations to executive shareholders on website metrics and performance and give guidance on how to achieve operational excellence.', icon: 'pi pi-cog'},
+                {company: 'Amyris', companyLogo: 'logo-amyris-colour.svg', date: '2022 - Present', blurb: 'Maintain tagging and analytics for major brands including but not limited to Biossance, JVN Hair and Pipette Baby. Migrated 50+ tags from these brands to Google Tag Manager. Build internal dashboard with Vue.JS and node to integrate with third party APIs (DataDog, Tag Inspector) to display critical website metrics, including a visual map of tags installed on the site and how they are loaded. Use this data to audit Shopify stores to improve site speed and overall performance (boost core web vital scores from 40-50 to 80). Give presentations to executive shareholders on website metrics and performance and give guidance on how to achieve operational excellence.', icon: 'pi pi-cog'},
                 {company: 'FloQast', companyLogo: 'FQ-logo.png', date: '2020-2022', blurb: 'Wordpress and PHP Web Development building out highly converting marketing pages. Gather project requirements from stakeholders, navigate ambiguity, and act as technical project manager and define scope and technical feasibility. Manage and configure A/B tests to evaluate user metrics and optimize website for maximum business value – boost signups for demos by 15%.', icon: 'pi pi-check'},
                 {company: 'Naadam', companyLogo: 'naadam.png', date: 'March 2020 - August 2020', blurb: 'Shopify related E-Commerce Development for luxury cashmere sweater brand - re-build Shopify theme from ground up utilizing ThemeKit. Managed integrations with third party APIs, coded custom checkout scripts for sale campaigns.', icon: 'pi pi-check'},
                 {company: 'Conductor', companyLogo: 'conductor.png', date: 'December 2019 - February 2020', blurb: 'Lead all Wordpress development initiatives for previously WeWork owned Conductor.com – conduct A/B testing on homepage and develop changes which resulted in a 30% increase in conversion rate.', icon: 'pi pi-check'},
